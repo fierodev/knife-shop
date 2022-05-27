c551eadf7458e76958271101e8d25969fc1e8aa3
fierodev <andriybigyn31@gmail.com>
2022-05-27 14:20:32 +0300

    header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,18 +8,24 @@ function Header(){
     return(
              <div className="header-dark">
             <nav className="navbar navbar-dark navbar-expand-md navigation-clean-search">
-                <div className="container"><a className="navbar-brand" href="#">Company Name</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-                    <div className="collapse navbar-collapse"
-                        id="navcol-1">
+                <div className="container"><a className="navbar-brand" href="#">Knifes shop</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navcol-1">
                         <ul className="nav navbar-nav">
                             <li className="nav-item" role="presentation"><a className="nav-link" href="#">Link</a></li>
                             <li className="dropdown"><a className="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown </a>
                                 <div className="dropdown-menu" role="menu"><a className="dropdown-item" role="presentation" href="#">First Item</a><a className="dropdown-item" role="presentation" href="#">Second Item</a><a className="dropdown-item" role="presentation" href="#">Third Item</a></div>
                             </li>
                         </ul>
-                        <form className="form-inline mr-auto" target="_self">
-                            <div className="form-group"><label htmlFor="search-field"><i className="fa fa-search"></i></label><input className="form-control search-field" type="search" name="search" id="search-field"/></div>
-                        </form><span className="navbar-text"><a href="#" className="login">Log In</a></span><a className="btn btn-light action-button" role="button" href="#">Sign Up</a></div>
+                       <div className="w-25 me-5"></div>
+                        <form className="form-inline mr-auto col-sm-4" target="_self">
+                            <input type="search" className="form-control form-control-dark " placeholder="Search..." aria-label="Search"></input>
+                        </form>
+                         <div className="p-2"></div>
+                        <span className="navbar-text">
+                            <a href="#" className="login">Log In</a>
+                        </span>
+                        <a className="btn btn-light action-button" role="button" href="#">Sign Up</a>
+                    </div>
                 </div>
             </nav>
             {/* <div className="container hero">
