2450bda5d23ef3d83aafacaefb72591f788bd560
fierodev <andriybigyn31@gmail.com>
2022-05-27 19:15:13 +0300

    goods in main

--- src/Main/CardLayout.js
+++ src/Main/CardLayout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-function CardLayout() {
+function CardLayout( {name, image, description} ) {
+
   return (
 
-    <div className="card" style={{width:"30rem"}}>
-        {/* <img src="..." className="card-img-top" alt="..." /> */}
-        <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="card ms-1 mt-1" style={{width:"30rem"}}>
+        <div className="card-body ">
+            <h5 className="card-title">{name}</h5>
+            <img className="w-50" src={image} />
+            <p className="card-text">{description}</p>
+            <a href="#" className="btn btn-primary">To buy</a>
         </div>
     </div>
   )
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,11 +4,33 @@ import '../App.css';
 import CardLayout from './CardLayout';
 
 function Main() {
+
+  const arrowOfObjects =[
+    {
+      name: 'Spyderco',
+      image: 'https://cdn.27.ua/sc--media--prod/default/c2/35/12/c23512e8-60c8-41e8-b6c0-c81bd4e73262.jpg',
+      description: 'the best knife'
+  },
+  {
+      name: 'Gerber',
+      image: 'https://images.prom.ua/3356747223_w700_h500_turisticheskij-skladnoj-nozh.jpg',
+      description: 'good knife'
+  },
+  {
+      name: 'Opinel',
+      image: 'https://cf.gorgany.com/catalog/product/cache/5f3efa0a8fec5fde77fda58c9d0d06d0/o/p/opinel_8_vrn_2042e632e29_16_bvt826broc8de5su.jpg',
+      description: 'good price'
+  },
+];
+
+
   return (
     <div className='Sidebar-block'>
         <Sidebar />
-        <div className='ms-3 mt-3'>
-          <CardLayout />
+        <div className='ms-5 mt-3 d-flex flex-wrap'>
+          {arrowOfObjects.map( prop => {
+            return <CardLayout name={prop.name} image={prop.image} description={prop.description} />
+            })}
         </div>
         
     </div>
